d4a08b23e8d6cf8d992b64f755b84a3aa1070bf2
elidrissi77 <idrissimov@gmail.com>
2026-08-04 00:50:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+document.body.style.opacity = "0";
+
+setTimeout(() => {
+document.body.style.transition = "opacity .8s";
+document.body.style.opacity = "1";
+},100);
+
+});
